d266cdf4eef7582bf29f14c02c3423d275b0eb80
Maooookai <1220568032@qq.com>
2021-08-07 01:12:10 +0800

    update reply logic add new sp bug fix

--- src/main/java/cn/maoookai/handler/GroupMessageEventHandler.java
+++ src/main/java/cn/maoookai/handler/GroupMessageEventHandler.java
@@ -80,43 +80,58 @@ public class GroupMessageEventHandler {
         if (messageContent.equals("抽卡")) {
             Image summonImage = fromGroup.uploadImage(ExternalResource.create(summonOne()));
             fromGroup.sendMessage(new At(event.getSender().getId()).plus("你召唤出了：").plus(summonImage));
+            return;
         }
         if (messageContent.equals("十连")) {
             ArrayList<File> result = summonTenTimes();
             File pic = ImageStitchUtil.bufferedToFile(result);
             Image summonImage = fromGroup.uploadImage(ExternalResource.create(pic));
             fromGroup.sendMessage(new At(event.getSender().getId()).plus("你召唤出了：").plus(summonImage));
+            return;
         }
 
         if (messageContent.equals("正能量") || messageContent.equals("每日一句") || messageContent.equals("学英语")) {
             DailyEnglishServiceImpl dailyEnglishService = new DailyEnglishServiceImpl();
             fromGroup.sendMessage(dailyEnglishService.today(properties.getProperty("daily.url")));
+            return;
         }
 
         if (messageContent.contains("就不能")) {
             if (RandomNumberUtil.getRandomNumber(100) > 50) {
-                Thread.sleep(2000);
+                Thread.sleep(5000);
                 event.getGroup().sendMessage("不能");
             }
+            return;
         }
 
         if (messageContent.contains("为什么")) {
             if (RandomNumberUtil.getRandomNumber(100) > 50) {
-                Thread.sleep(2000);
-                event.getGroup().sendMessage("不知道");
+                Thread.sleep(5000);
+                event.getGroup().sendMessage("因为，" + HttpGetUtil.getHttpPlainText("https://chp.shadiao.app/api.php"));
+            }
+            return;
+        }
+
+        for (String curse : properties.getProperty("curse").split(",")) {
+            if (messageContent.contains(curse)) {
+                event.getGroup().sendMessage(HttpGetUtil.getHttpPlainText("https://zuanbot.com/api.php?level=min&lang=zh_cn"));
+                return;
             }
         }
 
-        if (messageContent.equals("/fart"))
-            event.getGroup().sendMessage(HttpGetUtil.getHttpPlainText("https://chp.shadiao.app/api.php"));
-
-        if (messageContent.equals("/zuan"))
-            event.getGroup().sendMessage(HttpGetUtil.getHttpPlainText("https://zuanbot.com/api.php?level=min&lang=zh_cn"));
-
-        if (messageContent.matches("[a-zA-Z]+")) {
+        if (messageContent.matches("[a-zA-Z]+") && RandomNumberUtil.getRandomNumber(100) > 50) {
             JSONObject secretCode = JSONObject.fromObject(send("https://lab.magiconch.com/api/nbnhhsh/guess", new JSONObject().accumulate("text", messageContent)));
             event.getGroup().sendMessage(Objects.requireNonNull(getArray(secretCode, messageContent)).get(RandomNumberUtil.getRandomNumber(Objects.requireNonNull(getArray(secretCode, messageContent)).size())));
+            return;
         }
+
+        if (RandomNumberUtil.getRandomNumber(1000) < 5) {
+            Thread.sleep(5000);
+            event.getGroup().sendMessage(HttpGetUtil.getHttpPlainText("https://chp.shadiao.app/api.php"));
+        }
+
+
+
 
     }
 }
