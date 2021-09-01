f371058166ec44fc4bbcbd2263d38b9447fa61bf
Maooookai <1220568032@qq.com>
2021-09-01 10:44:48 +0800

    fix bug

--- src/main/java/cn/maoookai/scheduler/LogFileScheduler.java
+++ src/main/java/cn/maoookai/scheduler/LogFileScheduler.java
@@ -10,12 +10,12 @@ public class LogFileScheduler implements Runnable {
     public void run() {
         SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd-HH.mm.ss");
         Date date = new Date(System.currentTimeMillis());
-        File oldFile = new File("mirai.log");
+        File oldFile = new File("log/mirai.log");
         if (oldFile.exists()) {
-            if (oldFile.renameTo(new File(simpleDateFormat.format(date) + ".log"))) {
-                System.out.println("Old log is filed to " + simpleDateFormat.format(date) + ".log");
+            if (oldFile.renameTo(new File("log/"+simpleDateFormat.format(date) + ".log"))) {
+                System.out.println("Old log is filed to log/" + simpleDateFormat.format(date) + ".log");
             }
-            File newFile = new File("mirai.log");
+            File newFile = new File("log/mirai.log");
             try {
                 System.out.println(newFile.createNewFile());
             } catch (IOException e) {
