860c51808a776e08b4a8ef170f88ad865a78e776
2021112 <chenlm@datafine.cn>
2023-09-07 17:25:04 +0800

    修复登录问题

--- src/main/java/cn/maoookai/DamiMainApp.java
+++ src/main/java/cn/maoookai/DamiMainApp.java
@@ -6,7 +6,6 @@ import net.mamoe.mirai.Bot;
 import net.mamoe.mirai.BotFactory;
 import net.mamoe.mirai.auth.BotAuthorization;
 import net.mamoe.mirai.utils.BotConfiguration;
-import xyz.cssxsh.mirai.tool.FixProtocolVersion;
 
 import java.io.File;
 import java.io.FileInputStream;
@@ -25,38 +24,13 @@ public class DamiMainApp {
         LogFileScheduler logFileScheduler = new LogFileScheduler();
         ScheduledExecutorService executorService = Executors.newSingleThreadScheduledExecutor();
         executorService.scheduleAtFixedRate(logFileScheduler, 0, 24, TimeUnit.HOURS);
-//        Bot dami = BotFactory.INSTANCE.newBot(Long.parseLong(properties.getProperty("qq.account")), properties.getProperty("qq.password"), new BotConfiguration() {{
-//            setProtocol(MiraiProtocol.ANDROID_PAD);
-//            fileBasedDeviceInfo();
-//            File logDir = new File("log/");
-//            if (!logDir.exists() && !logDir.isDirectory()) {
-//                System.out.println("未发现日志文件夹，正在创建");
-//                if (logDir.mkdir())
-//                    System.out.println("已创建log文件夹");
-//                else
-//                    System.out.println("创建失败...请检查权限");
-//            }
-//            File oldLog = new File("log/mirai.log");
-//            if (oldLog.exists())
-//                System.out.println("Current log directory is " + oldLog.getAbsolutePath());
-//            redirectBotLogToFile(new File("log/mirai.log"));
-//        }});
-        Bot dami = BotFactory.INSTANCE.newBot(Long.parseLong(properties.getProperty("qq.account")), BotAuthorization.byQRCode(),new BotConfiguration() {{
-            setProtocol(MiraiProtocol.ANDROID_WATCH);
-            fileBasedDeviceInfo();
-            File logDir = new File("log/");
-            if (!logDir.exists() && !logDir.isDirectory()) {
-                System.out.println("未发现日志文件夹，正在创建");
-                if (logDir.mkdir())
-                    System.out.println("已创建log文件夹");
-                else
-                    System.out.println("创建失败...请检查权限");
-            }
-            File oldLog = new File("log/mirai.log");
-            if (oldLog.exists())
-                System.out.println("Current log directory is " + oldLog.getAbsolutePath());
-            redirectBotLogToFile(new File("log/mirai.log"));
-        }});
+
+        Bot dami = BotFactory.INSTANCE.newBot(Long.parseLong(properties.getProperty("qq.account")), BotAuthorization.byQRCode(), configuration -> {
+            configuration.setProtocol(BotConfiguration.MiraiProtocol.ANDROID_WATCH);
+            configuration.setHeartbeatStrategy(BotConfiguration.HeartbeatStrategy.STAT_HB);
+            configuration.fileBasedDeviceInfo();
+            configuration.redirectBotLogToDirectory(new File("log"));
+        });
         dami.login();
         new MainListener().initListener(dami, properties);
         dami.join();
